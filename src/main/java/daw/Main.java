398b14b106ba6e881d16a0c51d39eb58aa2989b3
Noelia <perezgonzaleznoelia08@gmail.com>
2023-12-02 18:56:13 +0100

    Creación clase main, creación de baraja

---
+++ src/main/java/daw/Main.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author noelia
+ * 
+ * Main: para probar lo que hemos hecho, 
+ * y también crear una baraja y mostrarla
+ * 
+ * 1. crear carta aleatoria y mostrar
+ * 2. crear baraja con constructor parametros 
+ * y mostrar
+ * 3. sacar cartas, mostrar hasta que salga 
+ * una carta que se solicita al usuario
+ * 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Cración de un naipe aleatorio
+        Naipe n1 = new Naipe();
+        System.out.println(n1);
+        System.out.println("--------------------------------------");
+        
+        //Creación de baraja completa
+        Baraja baraja = new Baraja();
+        
+        for (Naipe naipe : baraja.getBaraja()) {
+            System.out.println(naipe);
+        }
+        System.out.println("--------------------------------------");
+        
+    }
+    
+}
